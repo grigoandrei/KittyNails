f8a26a2e77dfd28835219938f62734ab5ecb1a3a
Andrei Grigorescu <andrei.grigorescu2017@gmail.com>
2026-05-20 21:37:43 +0200

    Registered both routes in main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from src.routers.appointments import router as appointments_router
+from src.routers.admin.services import router as admin_services_router
+from src.routers.services import router as services_router
 
 app = FastAPI()
 app.include_router(appointments_router)
+app.include_router(admin_services_router)
+app.include_router(services_router)
 
 @app.get("/")
 def health_check():
